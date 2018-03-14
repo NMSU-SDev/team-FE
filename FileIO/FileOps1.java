d2633525f0df58385f6f8d3e8a4e2e7bc8ab310a
Jacob Espinoza <35612420+jtespi@users.noreply.github.com>
2018-03-13 23:12:29 -0600

    Open file method updated, save file method now working
    The open file method now uses a dialog box and returns a file object.
    The save file method was renamed to write file and it also returns a file object.
    
    This code should now be ready to implement into the other code from the GUI and the XML parser.

--- FileIO/FileOps1.java
+++ FileIO/FileOps1.java
@@ -1,14 +1,21 @@
 import java.io.*;
 import java.nio.file.*;
 import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+
 import java.awt.Desktop;
 
-//import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE;
 
 /* Merrick Metadata Project
  * CS 371 Software Dev - Spring 2018
  * 2018 March 12 - 16
+ * 
+ * Basic File I/O operations code
+ * Revision 2a
+ * 
  * Author: Jacob Espinoza
  * 
  * Description: Basic File I/O operations that work on text files
@@ -75,9 +82,43 @@ public class FileOps1 {
 		
 	} // end create file stub method
 	
-	public static void saveFile ( File fileObj ) {
-		// TODO work on this
-		// ** more specification is needed
+	
+	/**
+	 * The writeFile method writes to the file that was specified.
+	 * In future versions, it will write XML code generated from a document object model.
+	 * 
+	 * @param fileObjIn a File object that will be written to.
+	 * @return a File object reference to the saved file or null if an error occurred.
+	 */
+	// an additional parameter will be a document object model object
+	public static File writeFile ( File fileObjIn ) {
+		
+		// initialize the saved file object to null at the start
+		File savedFile = null;
+		
+		Path pathToFile = fileObjIn.toPath();
+		
+		// some test output to write to the file
+		String content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" + 
+				"<note>\r\n" + 
+				"  <heading>Test File</heading>\r\n" + 
+				"  <body>This test XML file tests the file writing method.</body>\r\n" + 
+				"</note> ";
+		byte byteArray[] = content.getBytes();
+		
+		try( OutputStream out = new BufferedOutputStream(Files.newOutputStream(pathToFile, CREATE, APPEND))) {
+			out.write( byteArray, 0, byteArray.length );
+			System.out.println(" XML file creation successful!");
+		}
+		catch (IOException e ) {
+			System.err.println( e );
+			return savedFile;
+		}
+		
+		// no exceptions were handled by the catch block
+		// return a File object reference to the saved file
+		return ( savedFile = fileObjIn );
+		
 	}
 	
 	/**
@@ -85,32 +126,46 @@ public class FileOps1 {
 	 * Assumes the program is run on an OS with GUI support.
 	 * 
 	 * @param fileObj A File object that corresponds to the file to be opened.
+	 * @return a File object of the file that was opened or null if the file could not be opened.
 	 */
-	public static void openFile ( File fileObj ) {
-		// work on this
-		boolean fileExists1 = fileObj.exists();
+	public static File openFile ( ) {
 		
-		if ( fileExists1 == false ) {
-			System.err.println(" *** ERROR: File cannot be opened, it does not exist!");
-			return;
-		}
+		final JFileChooser fc = new JFileChooser();
+		int fileChooserReturnVal;
+		File fileSelected = null;
 		
 		// implementation 1: opens files using default programs
 		// ** mainly for testing, requires an OS with a GUI
 		if (!Desktop.isDesktopSupported() ) {
 			System.err.println(" *** ERROR: Desktop is not supported on this system.");
-			return;
+			return null;
+		}
+		// desktop is used for now to open the file in the OS' default program
+		Desktop d1 = Desktop.getDesktop();
+		
+		// will need to change parameter of showOpenDialog to the parent component of the dialog box
+		fileChooserReturnVal = fc.showOpenDialog(null);
+		fileSelected = fc.getSelectedFile();
+		if ( fileSelected != null ) {
+			System.out.printf("File selected = %s\n", fileSelected.toString() );
+		} else 
+			System.out.println("No file was selected.");
+		
+		
+		if ( fileChooserReturnVal == JFileChooser.APPROVE_OPTION ) {
+			try { d1.open( fileSelected ); }
+			catch (Exception e) { e.printStackTrace(); }
+			return fileSelected;
+		}
+		else if ( fileChooserReturnVal == JFileChooser.CANCEL_OPTION ) {
+			System.out.println("User cancelled file selection.");
+			return null;
+		}
+		else {
+			// the user closed the dialog or an error occurred.
+			return null;
 		}
 		
-		Desktop d1 = Desktop.getDesktop();
-		if ( fileExists1 ) {
-			try {
-				d1.open( fileObj );
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}		
 	} // end method openFile
 
 	public static void main(String[] args) {
@@ -120,12 +175,10 @@ public class FileOps1 {
 		
 		Path p = Paths.get("./test.xml");
 		Path p2;
-		File f1;
 		
         StringBuffer pathString1 = new StringBuffer( "./" );
-        StringBuffer file1s = new StringBuffer("./");
         
-        System.out.println("Enter a filename to create a blank file (include extension):");
+        System.out.println("Enter a filename to create a blank file in current directory (include extension):");
 
         pathString1.append( s.nextLine() );
         p2 = Paths.get( pathString1.toString() );
@@ -135,16 +188,13 @@ public class FileOps1 {
 		System.out.println("\nCalling create file stub function.....");
 		createFileStub( p );
 		
-		System.out.println("\nEnter a filepath to open the file (same directory) ");
+		System.out.println("\nAn open file dialog box has been created");
 		
-		file1s.append( s.nextLine() );
-		f1 = new File ( file1s.toString() );
-		
-		openFile( f1 );
+		openFile( );
 		System.err.flush();
 		s.close();
 		
-		System.out.println(" Done." );
+		System.out.println("  Done." );
 
 	}
 
